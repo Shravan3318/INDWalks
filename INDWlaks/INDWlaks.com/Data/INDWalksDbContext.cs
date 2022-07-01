83861d9923674f3630ae060cfe65ef1072ba56dc
shravan3318 <shetakeshravan50@gmail.com>
2022-07-01 14:45:15 +0530

    End Of Section 2

---
+++ INDWlaks/INDWlaks.com/Data/INDWalksDbContext.cs
@@ -0,0 +1,18 @@
+﻿using INDWlaks.com.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace INDWlaks.com.Data
+{
+    public class INDWalksDbContext: DbContext
+    {
+        public INDWalksDbContext (DbContextOptions<INDWalksDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walk { get; set; }
+
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
---
+++ INDWlaks/INDWlaks.com/Models/Domain/Walk.cs
@@ -0,0 +1,18 @@
+﻿namespace INDWlaks.com.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double length { get; set; }
+        public Guid RegionId { get; set; }
+
+        public Guid WalkDifficultyId { get; set; }
+
+        // Region Navigation Property
+
+        public Region Region { get; set; }
+
+        public WalkDifficulty WalkDifficulty { get; set; }
+    }
+}
